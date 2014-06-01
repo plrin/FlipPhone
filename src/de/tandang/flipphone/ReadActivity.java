9719e51f376a9d6d384b68f2a1c8c2b44d26a449
Mindleech <sven.eclipse91@gmail.com>
2014-06-01 17:37:02 +0200

    Added Gyroskop

--- src/de/tandang/flipphone/ReadActivity.java
+++ src/de/tandang/flipphone/ReadActivity.java
@@ -1,20 +1,29 @@
 package de.tandang.flipphone;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.os.Build;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
 
 public class ReadActivity extends Activity {
+	private static SensorManager sensorManager;
+	private Sensor sensor;
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +36,57 @@ public class ReadActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				stopRead();
-				switchAcitvity();
+				switchActivity();
 				
 			}
+			
+			
 		});
-
+		
+		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+		if (sensor != null) {
+		      sensorManager.registerListener(mySensorEventListener, sensor,
+		          SensorManager.SENSOR_DELAY_NORMAL);
+		      Log.i("Compass MainActivity", "Registerered for ORIENTATION Sensor");
+		    } else {
+		      Log.e("Compass MainActivity", "Registerered for ORIENTATION Sensor");
+		      Toast.makeText(this, "ORIENTATION Sensor not found",
+		          Toast.LENGTH_LONG).show();
+		      finish();
+		    }
 
 	}
+	
+	private SensorEventListener mySensorEventListener = new SensorEventListener() {
 
+	    @Override
+	    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	    }
 
+	    @Override
+	    public void onSensorChanged(SensorEvent event) {
+	    	
+	    	//Definiere TextFelder
+	    	TextView x = (TextView) findViewById(R.id.textView5);
+	    	TextView y = (TextView) findViewById(R.id.textView6);
+	    	TextView z = (TextView) findViewById(R.id.textView7);
+	    	//Definiere variablen für Gyroskop Ausgabe
+	    	float axisX = event.values[0];
+	        float axisY = event.values[1];
+	        float axisZ = event.values[2];
+	        //Ändere TextFelder
+	        x.setText("" + axisX );
+	        y.setText("" + axisY );
+	        z.setText("" + axisZ ); 
+	    }
+	  };
+	
+	
+	
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -50,7 +101,7 @@ public class ReadActivity extends Activity {
 		
 	}
 	
-	public void switchAcitvity() {
+	public void switchActivity() {
 		finish();
 		Intent intent = new Intent(this, ResultActivity.class);
 		startActivity(intent);
